980413fd85497588c29fdce1e56655c95cf8f11a
anirudh katuru <katuruanirudh1@gmail.com>
2026-04-01 15:27:22 +0530

    improved cart get-cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -74,20 +74,22 @@ const removeFromCart = async (req, res) => {
 
 
 // Get cart items
-
-
 const getCartItems = async (req, res) => {
-
-    try {
-        const userId = req.userId;
-        const userData = await userModel.findById(userId);
-        let cartData = await userData.cartData;
-        res.json({success:true,cartData})
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({success:false,message:error.message})  
+  try {
+    const userId = req.userId;
+    const userData = await userModel.findById(userId);
+    if (!userData) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
     }
-}
+    const cartData = userData.cartData; // no await needed
+    res.json({ success: true, cartData });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 
 
